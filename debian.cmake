6534ec1d108708e9e281262a8717c22ca34685a4
kevdillman <kcd33@uakron.edu>
2024-05-09 12:21:55 -0400

    Add system architecture check

--- debian.cmake
+++ debian.cmake
@@ -11,3 +11,10 @@ set(CPACK_DEB_COMPONENT_INSTALL ON)
 cmake_host_system_information(RESULT DISTRO_VERSION_ID QUERY DISTRIB_VERSION_ID)
 cmake_host_system_information(RESULT DISTRO_ID QUERY DISTRIB_ID)
 set(DEBIAN_VERSION "${DISTRO_ID}${DISTRO_VERSION_ID}")
+
+# Convert from processor type to Architecture
+if(CMAKE_SYSTEM_PROCESSOR EQUAL "aarch64")
+    set(DECOMPRESSOR_SYSTEM_ARCHITECTURE "arm64")
+else()
+    set(DECOMPRESSOR_SYSTEM_ARCHITECTURE "amd64")
+endif()
