a1161e2c5b6bbb5c22447a91a737a54153d35d58
kevdillman <kcd33@uakron.edu>
2024-05-09 12:15:39 -0400

    Move deb component flag into platform file

--- debian.cmake
+++ debian.cmake
@@ -1,1 +1,8 @@
-# cmake cpack file for debian packaging implementation
+# Debian packaging
+
+if(NOT DISTRO MATCHES "Ubuntu")
+    return()
+endif()
+
+# Separate packages for each component
+set(CPACK_DEB_COMPONENT_INSTALL ON)
