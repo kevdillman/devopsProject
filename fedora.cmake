deeb8fbf6529dc4f5c65e61a09779a9cb6f90120
kevdillman <kcd33@uakron.edu>
2024-05-09 17:41:25 -0400

    Add fedora component flag

--- fedora.cmake
+++ fedora.cmake
@@ -1,1 +1,8 @@
-# cmake cpack file for fedora implementation
+# Fedora packaging
+
+if(NOT DISTRO MATCHES "Fedora")
+    return()
+endif()
+
+# Separate packages for each component
+set(CPACK_RPM_COMPONENT_INSTALL ON)
