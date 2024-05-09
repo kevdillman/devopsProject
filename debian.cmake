8cbe3b6070832f8a06f8f7a167935b3b5024ddd4
kevdillman <kcd33@uakron.edu>
2024-05-09 12:17:45 -0400

    Add project version to cmake files

--- debian.cmake
+++ debian.cmake
@@ -6,3 +6,8 @@ endif()
 
 # Separate packages for each component
 set(CPACK_DEB_COMPONENT_INSTALL ON)
+
+# Distribution version, e.g., ubuntu24.04
+cmake_host_system_information(RESULT DISTRO_VERSION_ID QUERY DISTRIB_VERSION_ID)
+cmake_host_system_information(RESULT DISTRO_ID QUERY DISTRIB_ID)
+set(DEBIAN_VERSION "${DISTRO_ID}${DISTRO_VERSION_ID}")
